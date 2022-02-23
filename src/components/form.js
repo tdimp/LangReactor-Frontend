3f0744a331655f7de922b99002599804a697e054
Thomas Dimperio <twdimperio@gmail.com>
2022-02-23 12:26:00 -0700

    Add basic English to German translation functionality in the console.

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
+function handleSubmit(e) {
+    e.preventDefault();
+    const text = e.target.childNodes[0].value
+    fetch(`https://api-free.deepl.com/v2/translate?auth_key=8b604fe9-81cc-803f-72f3-cfcff7236ae4:fx&text=${text}&target_lang=DE`)
+        .then(r => r.json())
+        .then(data => console.log(data.translations[0].text))
+    
+}
+
 function Form() {
     const [input, setInput] = useState("hello");
     
     return (
-        <form>
-            <input type="text" value={input} />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search something" />
+            <button>Search</button>
         </form>
     );
 }
 
 export default Form;
-
-
-
-//const germanURL = `https://lt-translate-test.herokuapp.com/?langpair=en-de&query=${input}&wortart=${type}`
-//    fetch(germanURL)
--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,9 @@
 import React, {useEffect} from "react";
 import Form from "./form";
 
-const germanURL = `https://lt-translate-test.herokuapp.com/?langpair=en-de&query=change&wortart=VERB`
+//const germanURL = `https://lt-translate-test.herokuapp.com/?langpair=en-de&query=change&wortart=VERB`
 
 function Home() {
-    useEffect(() => {
-        fetch(germanURL)
-            .then((r) => r.json())
-            .then((data) => console.log(data))
-    })
-
     return (
         <div>
             <h1>Home!</h1>
